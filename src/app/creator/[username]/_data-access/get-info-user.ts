9ac7d509c838169e7aebe7bab01267f69592bf8b
marvieiradev <marcosvieira482@gmail.com>
2025-12-02 20:23:37 -0300

    feat: adds information retrieval functionality to the creator page

---
+++ src/app/creator/[username]/_data-access/get-info-user.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { z } from "zod";
+
+const createUsernameSchema = z.object({
+  username: z.string({ message: "Nome de usuário é obrigatório" }),
+});
+
+type CreateUsernameSchema = z.infer<typeof createUsernameSchema>;
+
+export async function getInfoUser(data: CreateUsernameSchema) {
+  const schema = createUsernameSchema.safeParse(data);
+
+  if (!schema.success) {
+    return null;
+  }
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { username: data.username },
+      select: {
+        id: true,
+        name: true,
+        username: true,
+        bio: true,
+        image: true,
+      },
+    });
+    return user;
+  } catch (error) {
+    return null;
+  }
+}
